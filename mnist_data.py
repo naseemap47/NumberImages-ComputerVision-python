af0d1b2bed19a9d1cfc3f0d0fcc6ed721c0456e9
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-08 14:17:42 +0530

    created display_some_samples and display some sample data

--- mnist_data.py
+++ mnist_data.py
@@ -1,4 +1,20 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def display_some_samples(examples, labels):
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        idx = np.random.randint(0, examples.shape[0]-1)
+        img = examples[idx]
+        label = labels[idx]
+        plt.subplot(5, 5, i+1)
+        plt.title(str(label))
+        plt.tight_layout()
+        plt.imshow(img, cmap='gray')
+        plt.axis('off')
+    plt.show()
 
 if __name__=='__main__':
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -7,3 +23,5 @@ if __name__=='__main__':
     print("y_train Shape = ", y_train.shape)
     print("x_test Shape = ", x_test.shape)
     print("y_test Shape = ", y_test.shape)
+
+    display_some_samples(x_train, y_train)
